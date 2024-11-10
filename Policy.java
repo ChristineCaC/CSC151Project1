9b93bfcbadc131b8e1bc6f3ddd3b8495d5493b49
ChristineCaC <cchapm17@email.cpcc.edu>
2024-11-10 17:44:27 -0500

    Update Policy.java
    Added static field to track number of Policy objects

--- Policy.java
+++ Policy.java
@@ -4,11 +4,17 @@ public class Policy {
     private String providerName; 
     private PolicyHolder policyHolder; 
 
+    // Static field to track the number of Policy objects created
+    private static int numberOfPolicies = 0;
+
     // Default constructor that initializes all fields to default values.
     public Policy() {
         policyNumber = "";
         providerName = "";
         policyHolder = new PolicyHolder(); // Initializes the PolicyHolder object.
+        
+        // Increment the static field every time a Policy object is created
+        numberOfPolicies++;
     }
 
     /**
@@ -19,10 +25,13 @@ public class Policy {
       @param holder The policyholder associated with the policy.
     */
      public Policy(String pNumber, String pName, PolicyHolder holder) {
-        policyNumber = pNumber;
-        providerName = pName;
-        policyHolder = holder;
-    }
+         policyNumber = pNumber;
+         providerName = pName;
+         policyHolder = holder;
+        
+         // Increment the static field every time a Policy object is created
+         numberOfPolicies++;
+     }
 
     // Setters with @param tags
     public void setPolicyNumber(String pNumber) { policyNumber = pNumber; }
@@ -34,26 +43,31 @@ public class Policy {
     public String getProviderName() { return providerName; }
     public PolicyHolder getPolicyHolder() { return policyHolder; }
 
+    // Getter for the number of Policy objects created
+    public static int getNumberOfPolicies() {
+        return numberOfPolicies;
+    }
+
     /**
       Calculates and returns the price of the policy based on the policyholder's information.
       
       @return The calculated price of the policy.
     */
      public double getPrice() {
-        final double BASE_PRICE = 600;
-        final double ADDITIONAL_FEE_AGE = 75;
-        final double ADDITIONAL_FEE_SMOKING = 100;
-        final double ADDITIONAL_FEE_PER_BMI = 20;
-        final int AGE_THRESHOLD = 50;
-        final int BMI_THRESHOLD = 35;
+         final double BASE_PRICE = 600;
+         final double ADDITIONAL_FEE_AGE = 75;
+         final double ADDITIONAL_FEE_SMOKING = 100;
+         final double ADDITIONAL_FEE_PER_BMI = 20;
+         final int AGE_THRESHOLD = 50;
+         final int BMI_THRESHOLD = 35;
 
-        double price = BASE_PRICE;
-        if (policyHolder.getAge() > AGE_THRESHOLD) price += ADDITIONAL_FEE_AGE;
-        if (policyHolder.getSmokingStatus().equalsIgnoreCase("smoker")) price += ADDITIONAL_FEE_SMOKING;
-        if (policyHolder.getBMI() > BMI_THRESHOLD) price += ((policyHolder.getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI);
+         double price = BASE_PRICE;
+         if (policyHolder.getAge() > AGE_THRESHOLD) price += ADDITIONAL_FEE_AGE;
+         if (policyHolder.getSmokingStatus().equalsIgnoreCase("smoker")) price += ADDITIONAL_FEE_SMOKING;
+         if (policyHolder.getBMI() > BMI_THRESHOLD) price += ((policyHolder.getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI);
 
-        return price;  // Close the getPrice method here
-    }
+         return price;
+     }
 
     /**
       Returns a string representation of the Policy object.
@@ -63,5 +77,5 @@ public class Policy {
      @Override
      public String toString() {
          return "Policy [policyNumber=" + policyNumber + ", providerName=" + providerName + ", policyHolder=" + policyHolder.toString() + "]";
-    }
+     }
 }
